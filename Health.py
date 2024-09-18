397e5168971926d2c623a1aff1fc5345c5aaee9e
Travis <127796972+WebDev993@users.noreply.github.com>
2024-09-17 20:49:54 -0400

    Update Health.py

--- Health.py
+++ Health.py
@@ -4,13 +4,13 @@ import random
 health = 50
 difficulty = 2
 
-#random number generation
+#random number generation between 25-50 for potion health 
 potionHealth = int(random.randint (25,50)/ difficulty)
 
 
-#adding potionHealth to Health
+#adding health to potion health
 health = health + potionHealth
 
 
-#showing new health
+#showing new health of the player
 print(health)
